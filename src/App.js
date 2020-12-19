eb6cb34b9f6b00418dfeb364725e5f955f636b59
ally-sorloth <allysorloth@gmail.com>
2020-12-19 03:40:08 +0300

    create chart and countrypicker

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+
+import styles from './App.module.css';
+
+import { fetchData } from './api';
+
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+
+  async componentDidMount() {
+
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async (country) => {
+    console.log(country);
+    //fetch the data
+    //set the data
+  }
+  
+  render(){
+
+    const { data } = this.state;
+
+    return (
+
+      <div className={styles.container}>
+        <Cards data={data} />
+        <Chart/>
+        <CountryPicker handleCountryChange={this.handleCountryChange}/>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
